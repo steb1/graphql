23d815bd837f6e583e3ce660f680457e60ee80b7
lomalack <you@example.com>
2024-01-22 21:26:52 +0000

    fix : displayUserPage

--- script.js
+++ script.js
@@ -4,12 +4,7 @@ var jwtToken = ""
     try {
         const username = document.getElementById("username");
         const password = document.getElementById("password");
-    
-        let usernameValue = username.value
-        let passwordValue = password.value
-        
-        console.log("info", usernameValue, passwordValue);
-    
+            
         if (username && password) {
             getToken().then(isValid => {
                 if (isValid) {
@@ -18,7 +13,6 @@ var jwtToken = ""
                 } else {
                   let elem = document.getElementById("error")
                   elem.style.visibility = "visible"
-                  console.log("errooooooooooooor");
                 }
             })
             .catch (() => {
@@ -253,7 +247,7 @@ var points = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
   <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png">
   <link rel="icon" type="image/png" href="../assets/img/favicon.png">
   <title>
-    Soft UI Dashboard by Creative Tim
+    
   </title>
   <!--     Fonts and icons     -->
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
@@ -275,7 +269,7 @@ var points = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
     <div class="collapse navbar-collapse w-auto " id="sidenav-collapse-main">
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link  active" href="../pages/dashboard.html">
+          <a class="nav-link  active" href="#">
             <div class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
               <svg width="12px" height="12px" viewBox="0 0 45 40" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
                 <title>shop </title>
@@ -489,24 +483,7 @@ var points = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
               <div class="container border-radius-lg">
                 <div class="row">
                   <div class="col-3 py-3 ps-0">
-                    <div class="d-flex mb-2">
-                      <div class="icon icon-shape icon-xxs shadow border-radius-sm bg-gradient-primary text-center me-2 d-flex align-items-center justify-content-center">
-                        <svg width="10px" height="10px" viewBox="0 0 40 44" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-                          <title>document</title>
-                          <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-                            <g transform="translate(-1870.000000, -591.000000)" fill="#FFFFFF" fill-rule="nonzero">
-                              <g transform="translate(1716.000000, 291.000000)">
-                                <g transform="translate(154.000000, 300.000000)">
-                                  <path class="color-background" d="M40,40 L36.3636364,40 L36.3636364,3.63636364 L5.45454545,3.63636364 L5.45454545,0 L38.1818182,0 C39.1854545,0 40,0.814545455 40,1.81818182 L40,40 Z" opacity="0.603585379"></path>
-                                  <path class="color-background" d="M30.9090909,7.27272727 L1.81818182,7.27272727 C0.814545455,7.27272727 0,8.08727273 0,9.09090909 L0,41.8181818 C0,42.8218182 0.814545455,43.6363636 1.81818182,43.6363636 L30.9090909,43.6363636 C31.9127273,43.6363636 32.7272727,42.8218182 32.7272727,41.8181818 L32.7272727,9.09090909 C32.7272727,8.08727273 31.9127273,7.27272727 30.9090909,7.27272727 Z M18.1818182,34.5454545 L7.27272727,34.5454545 L7.27272727,30.9090909 L18.1818182,30.9090909 L18.1818182,34.5454545 Z M25.4545455,27.2727273 L7.27272727,27.2727273 L7.27272727,23.6363636 L25.4545455,23.6363636 L25.4545455,27.2727273 Z M25.4545455,20 L7.27272727,20 L7.27272727,16.3636364 L25.4545455,16.3636364 L25.4545455,20 Z"></path>
-                                </g>
-                              </g>
-                            </g>
-                          </g>
-                        </svg>
-                      </div>
-                      <p class="text-xs mt-1 mb-0 font-weight-bold">Note</p>
-                    </div>
+                   
                     <h4 class="font-weight-bolder"></h4>
                     <div>
                     🟢 Pass (${data.data.auditPass.aggregate.count}) <br>
@@ -636,7 +613,7 @@ var points = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
         </span>
         <div class="timeline-content">
         <h6 class="text-dark text-sm font-weight-bold mb-0">${ (element.path.split("/")[3]).charAt(0).toUpperCase() + (element.path.split("/")[3]).slice(1)}</h6> 
-        <p class="text-secondary font-weight-bold text-xs mt-1 mb-0">Finised ${`${year}-${month < 10 ? '0' + month : month}-${day < 10 ? '0' + day : day}`} </p>
+        <p class="text-secondary font-weight-bold text-xs mt-1 mb-0">Finihsed ${`${year}-${month < 10 ? '0' + month : month}-${day < 10 ? '0' + day : day}`} </p>
           <p class="text-secondary font-weight-bold text-xs mt-1 mb-0" ></p>
         </div>
       </div>
@@ -675,7 +652,7 @@ var points = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
           </div>
         </div>
         <div class="progress">
-          <div class="progress-bar bg-gradient-info w-${element.amount}" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"> 
+          <div class="progress-bar bg-gradient-info w-${formatPercent(element.amount)}" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"> 
         </div>
         <div>${element.amount}% </div class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7"> 
       </div>
@@ -686,7 +663,7 @@ var points = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
   ////////////////////////////
   
   // Set up SVG dimensions
-  const svgWidth = 400;
+  const svgWidth = 500;
   const svgHeight = 450;
   const margin = { top: 20, right: 20, bottom: 50, left: 50 };
 
@@ -783,8 +760,10 @@ var points = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
       window.location.reload()
     })
 })
+}
 
-  
+let formatPercent = (percent) => {
+  return Math.round(percent / 10) * 10
 }
 
 displaySignInPage()
@@ -979,7 +958,3 @@ async function getData () {
   }
 
 }
-
-
-
-
